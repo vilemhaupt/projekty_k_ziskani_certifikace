a2b92cde1590ca4f33b02cc3e9a5dd7047a3d166
vilemhaupt <71026000+vilemhaupt@users.noreply.github.com>
2020-09-15 15:41:38 +0200

    Update validator_cisel.js

--- javascript/validator_cisel.js
+++ javascript/validator_cisel.js
@@ -1,12 +1,12 @@
-<!--
 
-Tento projekt byl použit k získání certifikace
 
-JavaScript Algorithms and Data Structures Certification 
+// Tento projekt byl použit k získání certifikace
 
-na freecodecamp.org
+// JavaScript Algorithms and Data Structures Certification 
 
--->
+// na freecodecamp.org
+
+
 
 // zkouska je vyznačení neplatných znaků v telefonním čísle, pokud se najde některý z těchto symbolů, bude return false;
 var zkouska = /^([+]?1[\s]?)?((?:[(](?:[2-9]1[02-9]|[2-9][02-8][0-9])[)][\s]?)|(?:(?:[2-9]1[02-9]|[2-9][02-8][0-9])[\s.-]?)){1}([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2}[\s.-]?){1}([0-9]{4}){1}$/;
